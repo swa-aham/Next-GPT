f1c1c1f2f24710f709a3efa57ea87ead3a8520cb
Swa-aham <sohammandaviya@gmail.com>
2024-09-08 02:55:33 +0530

    Sign in and sign up

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next GPT <> Sign-In",
+};
+export default function SignInPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignIn appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+    </div>
+  );
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next GPT <> Sign-Up",
+};
+export default function SignUpPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUp appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+    </div>
+  );
+}
